881b5b237045177c0dbc2ee03f26d202d1c645c2
peng-xiao-shuai <1612565136@qq.com>
2024-02-01 18:22:13 +0800

    perf: 页面补充

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,25 +1,46 @@
 'use client';
+import logo from '/public/logo4.png';
 import { useState } from 'react';
-import { useRoomStore } from '@/hooks/use-room-data';
-import { debounce } from '@/utils/debounce-throttle';
 import { Dialog, HomeForm } from './components';
 import { useTranslation } from '@/locales/client';
-import Link from 'next/link';
+import { HOME_KEYS } from '@@/locales/keys';
+import Image from 'next/image';
 
 export default function Home({ params: { lng } }: CustomReactParams) {
   const [roomStatus, setRoomStatus] = useState<'ADD' | 'JOIN'>('ADD');
-  const { encryptData } = useRoomStore();
   const [visible, setVisible] = useState(false);
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
+
+  // 点击加入/创建房间按钮
+  const handleRoom = (type: 'JOIN' | 'ADD') => {
+    setRoomStatus(type);
+    setVisible(true);
+  };
 
   return (
     <>
-      {/* <button onClick={signin}>连接</button> */}
-      <Link href="/sign-up">连接</Link>
+      <div className="flex flex-wrap h-[82vh] items-center">
+        <Image
+          className="w-32 h-32 m-auto mask mask-squircle"
+          src={logo}
+          alt="Logo"
+        />
 
-      <button onClick={() => debounce(setVisible, 300, [true], true)}>
-        连接欧易
-      </button>
+        <div className="flex my-auto m-t w-[100%] justify-around">
+          <button
+            className="w-[43%] btn btn-outline border-base-content text-base-content"
+            onClick={() => handleRoom('JOIN')}
+          >
+            {t(HOME_KEYS.SELECT_ROOM)}
+          </button>
+          <button
+            className="w-[43%] btn btn-primary btn-active"
+            onClick={() => handleRoom('ADD')}
+          >
+            {t(HOME_KEYS.CREATE_ROOM)}
+          </button>
+        </div>
+      </div>
 
       <Dialog visible={visible} setVisible={setVisible}>
         <div className="bg-base-300 flex flex-wrap justify-center">
