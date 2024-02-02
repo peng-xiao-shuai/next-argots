a76bd6f6258606b3ba5d24ea76a0be5654dd1f21
peng-xiao-shuai <1612565136@qq.com>
2024-02-02 17:46:18 +0800

    feat: 新增 payload。补充集合配置

---
+++ src/server/collections/Feedback.ts
@@ -0,0 +1,105 @@
+/**
+ * 反馈
+ */
+import { CollectionBeforeChangeHook, CollectionConfig } from 'payload/types';
+import { FeedbackRecord } from '../payload/payload-types';
+import { getPayloadClient } from '../payload/get-payload';
+
+const beforeChange: CollectionBeforeChangeHook<FeedbackRecord> = async ({
+  data,
+}) => {
+  // const payload = await getPayloadClient();
+  // data.operateType = (data.count || 0) >= 0 ? 'added' : 'reduce';
+
+  // const result = await payload.findByID({
+  //   collection: 'users', // required
+  //   id: data.userId as any, // required
+  // });
+
+  // await payload.update({
+  //   collection: 'users', // required
+  //   id: data.userId as any, // required
+  //   data: {
+  //     ...result,
+  //     points: (result.points || 0) + data.count!,
+  //   },
+  // });
+
+  return data;
+};
+
+export const Feedback: CollectionConfig = {
+  slug: 'feedback-record',
+  fields: [
+    {
+      name: 'email',
+      type: 'text',
+      admin: {
+        readOnly: true,
+      },
+    },
+    {
+      // 回复状态
+      name: 'status',
+      type: 'select',
+      defaultValue: '0',
+      options: [
+        {
+          label: 'no reply',
+          value: '0',
+        },
+        {
+          label: 'replied',
+          value: '1',
+        },
+      ],
+      admin: {
+        condition: () => false,
+      },
+    },
+    {
+      name: 'type',
+      type: 'select',
+      required: true,
+      admin: {
+        readOnly: true,
+      },
+      options: [
+        {
+          label: 'issus',
+          value: '1',
+        },
+        {
+          label: 'opinion',
+          value: '2',
+        },
+        {
+          label: 'i want to',
+          value: '3',
+        },
+      ],
+    },
+    {
+      name: 'content',
+      type: 'text',
+      required: true,
+      admin: {
+        readOnly: true,
+      },
+    },
+    {
+      name: 'replyContent',
+      type: 'richText',
+      admin: {},
+    },
+  ],
+  hooks: {
+    beforeChange: [beforeChange],
+  },
+  access: {
+    read: () => true,
+    delete: () => false,
+    create: () => false,
+    update: ({ req: { user } }) => user.role === 'admin',
+  },
+};
---
+++ src/server/payload/payload.config.ts
@@ -0,0 +1,54 @@
+/**
+ * 配置 payload CMS 无头内容管理系统
+ * @author peng-xiao-shuai
+ * @see https://www.youtube.com/watch?v=06g6YJ6JCJU&t=8070s
+ */
+
+import path from 'path';
+import { mongooseAdapter } from '@payloadcms/db-mongodb';
+import { webpackBundler } from '@payloadcms/bundler-webpack';
+import { slateEditor } from '@payloadcms/richtext-slate';
+import { buildConfig } from 'payload/config';
+import { Users } from '../collections/Users';
+import { Feedback } from '../collections/Feedback';
+
+export default buildConfig({
+  // 设置服务器的 URL，从环境变量 NEXT_PUBLIC_SERVER_URL 获取。
+  serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
+  collections: [Users, Feedback],
+  admin: {
+    user: 'users',
+    // 设置用于 Payload CMS 管理界面的打包工具，这里使用了
+    bundler: webpackBundler(),
+    // 配置管理系统 Meta
+    meta: {
+      titleSuffix: 'Payload manage',
+    },
+    autoLogin: {
+      email: '1612565136@qq.com',
+      password: 'test',
+      prefillOnly: true,
+    },
+    components: {
+      views: {},
+    },
+    // css: path.resolve(__dirname, '../../styles/payload-tailwindcss.css'),
+  },
+  debug: process.env.NODE_ENV === 'production' ? false : true,
+  // 定义路由，例如管理界面的路由。
+  routes: {
+    admin: '/admin',
+  },
+  // 设置富文本编辑器，这里使用了 Slate 编辑器。
+  editor: slateEditor({}),
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+  rateLimit: {
+    max: 500,
+    trustProxy: false,
+  },
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URL!,
+  }),
+});
