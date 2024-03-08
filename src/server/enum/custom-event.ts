c37e0f63b48be5ec912c39b91347af8c76c07235
peng-xiao-shuai <1612565136@qq.com>
2024-03-08 16:12:13 +0800

    pref: Code format optimization

--- src/server/enum/custom-event.ts
+++ src/server/enum/custom-event.ts
@@ -1,5 +1,3 @@
 export enum CustomEvent {
   RECEIVE_INFORMATION = 'receive_information',
-
-  SIGN_ERROR = 'sign_error',
 }
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,3 +1,5 @@
+import { toast } from 'sonner';
+
 export const fetchReq = async (
   url: string,
   body: object,
@@ -21,6 +23,9 @@ export const fetchReq = async (
     return data;
   } catch (err: any) {
     console.log(url, '报错：', err);
+    if (err.message) {
+      toast.error(err.message);
+    }
     throw new Error(err.message);
   }
 };
