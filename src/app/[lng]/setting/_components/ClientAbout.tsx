5482513e4ad198875d33ccd50c02e5cef0e13352
peng-xiao-shuai <1612565136@qq.com>
2024-03-19 00:18:44 +0800

    fix: feedback 404

--- src/app/[lng]/setting/_components/ClientAbout.tsx
+++ src/app/[lng]/setting/_components/ClientAbout.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import { FC, useContext } from 'react';
+import { FC, useContext, useEffect, useState } from 'react';
 import { AiOutlineRight } from 'react-icons/ai';
 import { useTranslation } from '@/locales/client';
 import { COMMON_KEYS } from '@@/locales/keys';
@@ -11,40 +11,52 @@ type AboutList = {
   path?: string;
   click?: (item: AboutList) => void;
 };
-const aboutList: AboutList[] = [
-  // {
-  //   label: '检查更新',
-  //   locale: COMMON_KEYS.CHECK_FOR_UPDATES,
-  //   click(item) {
-  //     console.log(item.label);
-  //   },
-  // },
-  {
-    label: '意见反馈',
-    locale: COMMON_KEYS.FEEDBACK,
-    path: '/setting/about/feedback',
-  },
-  // {
-  //   label: '隐私政策',
-  //   locale: 'Privacy.policy',
-  //   path: '/issue-opinion',
-  // },
-  {
-    label: '邮件联系',
-    locale: COMMON_KEYS['E-MAIL_CONTACT'],
-    click(item) {
-      const recipient = 'pxs161256513@gmail.com';
-
-      const mailtoLink = `mailto:${encodeURIComponent(recipient)}`;
-
-      window.location.href = mailtoLink;
-    },
-  },
-];
 
 export const ClientAboutMenu: FC = () => {
   const { t } = useTranslation();
+  const [aboutList, setAboutList] = useState<AboutList[]>([
+    // {
+    //   label: '检查更新',
+    //   locale: COMMON_KEYS.CHECK_FOR_UPDATES,
+    //   click(item) {
+    //     console.log(item.label);
+    //   },
+    // },
+    {
+      label: '意见反馈',
+      locale: COMMON_KEYS.FEEDBACK,
+      path: '/setting/about/feedback',
+    },
+    // {
+    //   label: '隐私政策',
+    //   locale: 'Privacy.policy',
+    //   path: '/issue-opinion',
+    // },
+    {
+      label: '邮件联系',
+      locale: COMMON_KEYS['E-MAIL_CONTACT'],
+      click(item) {
+        const recipient = 'pxs161256513@gmail.com';
+
+        const mailtoLink = `mailto:${encodeURIComponent(recipient)}`;
+
+        window.location.href = mailtoLink;
+      },
+    },
+  ]);
   const router = useRouter();
+  useEffect(() => {
+    const copyLists = aboutList.map((item) => ({
+      ...item,
+      path: item.path
+        ? item.path.includes(location?.pathname)
+          ? item.path
+          : location.pathname + item.path
+        : '',
+    }));
+
+    setAboutList(copyLists);
+  }, [aboutList]);
   return (
     <>
       {aboutList.map((item, index) => (
