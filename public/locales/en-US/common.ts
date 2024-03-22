067ea10972cb231aecc574a9a60d5c6967f1bf59
peng-xiao-shuai <1612565136@qq.com>
2024-03-23 00:17:08 +0800

    pref: i18n desc

--- public/locales/en-US/common.ts
+++ public/locales/en-US/common.ts
@@ -4,7 +4,7 @@ export const COMMON = {
   [COMMON_KEYS.PACKAGE_NAME]: 'Argots',
 
   [META.DESC]:
-    'A free chat platform that encrypts conversation information throughout the process to protect your security and privacy. No information is collected from you and no permissions are required from you.',
+    'A free chat platform that encrypts conversation information throughout the process to protect your security and privacy. No information is collected from you and no permissions are required from you. End of chat Clear all records',
   [META.KEYWORDS]:
     'help channels,room,information encryption,no access,Open chat,end-to-end',
 };
