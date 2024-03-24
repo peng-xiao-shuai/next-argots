858aeedbc6dd7b979ba63ccac18025b71c3d0fa3
peng-xiao-shuai <1612565136@qq.com>
2024-03-24 19:38:06 +0800

    fix: No theme is set as the default theme

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -83,7 +83,7 @@ export default async function RootLayout({
                 isDark ? 'dark' : 'light'
               );
             } else {
-              document.documentElement.setAttribute('data-theme', dataTheme);
+              document.documentElement.setAttribute('data-theme', dataTheme || 'dark');
             }
           `,
           }}
