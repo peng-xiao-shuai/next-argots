d395d91f7ce830e0cc79a9f74ea4ab7cd4558ed2
peng-xiao-shuai <1612565136@qq.com>
2024-03-26 21:00:00 +0800

    pref: sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,30 @@
+const paths = ['/', '/setting', '/setting/theme-change', '/setting/size-change', '/setting/lang-change', '/setting/about', '/setting/about/feedback', '/chat-room']
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL,
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  additionalPaths: (config) => {
+    const result = []
+
+    paths.forEach(item => {
+      const alternateRefs = ['ja-JP', 'zh-TW', 'en-US', 'zh-CN'].map(lng => ({
+        href: process.env.SITE_URL + '/' + lng,
+        hreflang: lng,
+      }))
+
+      result.push({
+        loc: item,
+        changefreq: 'daily',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+
+        alternateRefs: alternateRefs
+      })
+    })
+
+    console.log(result)
+
+    return result
+  }
+}
