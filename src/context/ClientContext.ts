a11c20c2015d755155bb28d38debcc029e33fb8e
peng-xiao-shuai <1612565136@qq.com>
2024-04-19 11:17:19 +0800

    pref: Add attributes in context

--- src/context/ClientContext.ts
+++ src/context/ClientContext.ts
@@ -1,12 +1,14 @@
 'use client';
 
+import { LinkUserInfo } from '@/app/[lng]/chat-room/_components/Client';
+import { type Data } from '@/app/api/join-link/route';
 import { type Lng } from '@/locales/i18n';
-import { type Data } from 'payload/types';
 import { createContext } from 'react';
 
 export const ClientChatContext = createContext<{
   lng: Lng;
   joinData?: Data;
+  userInfo?: LinkUserInfo;
 }>({
   lng: 'en-US',
 });
