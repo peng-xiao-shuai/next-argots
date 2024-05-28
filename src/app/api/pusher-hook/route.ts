f1fcde11f371923b5cb847c2472229b4616285ef
peng-xiao-shuai <1612565136@qq.com>
2024-05-28 18:34:44 +0800

    pref: Perfect webhook

--- src/app/api/pusher-hook/route.ts
+++ src/app/api/pusher-hook/route.ts
@@ -1,17 +1,17 @@
+import clientPromise from '@/server/db';
+import { InviteLink, Room } from '@/server/payload/payload-types';
 import { createHmac } from 'crypto';
 import { NextRequest } from 'next/server';
 
 const handler = async (req: NextRequest) => {
-  const body = await req.json();
-  console.log(body);
+  const body = (await req.json()) as {
+    time_ms: number;
+    events: { name: string; channel: string }[];
+  };
 
   if (req.headers.get('user-agent') === 'pusher-webhooks') {
     const receivedSignature = req.headers.get('x-pusher-signature');
-    const receivedKey = req.headers.get('x-pusher-key');
-    console.log(
-      'receivedSignature:' + receivedSignature,
-      'receivedKey:' + receivedKey
-    );
+    // const receivedKey = req.headers.get('x-pusher-key');
 
     const payload = JSON.stringify(body);
 
@@ -23,7 +23,31 @@ const handler = async (req: NextRequest) => {
       .digest('hex');
 
     if (receivedSignature === expectedSignature) {
+      const client = await clientPromise;
+      const collection = client
+        .db(process.env.DATABASE_DB)
+        .collection<Room>('rooms');
+
+      const linkCollection = client
+        .db(process.env.DATABASE_DB)
+        .collection<InviteLink>('invite-link');
+
       console.log('Valid webhook');
+      body.events.forEach(async (event) => {
+        if (event.channel.includes('presence-')) {
+          try {
+            const room = await collection.findOneAndDelete({
+              channel: event.channel,
+            });
+
+            linkCollection.deleteMany({
+              roomId: room.value?.id,
+            });
+          } catch (err) {
+            console.log(err, '删除报错');
+          }
+        }
+      });
       // 处理有效的 webhook 逻辑
       return new Response(
         JSON.stringify({
@@ -45,7 +69,6 @@ const handler = async (req: NextRequest) => {
       );
     }
   }
-  return Response.json({ a: 1 });
 };
 
 export { handler as POST };
