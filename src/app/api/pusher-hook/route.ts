1f5c6f0d2165e42fb83192e6ba4bd7667eeae5e1
peng-xiao-shuai <1612565136@qq.com>
2024-05-28 11:25:02 +0800

    pref: log

--- src/app/api/pusher-hook/route.ts
+++ src/app/api/pusher-hook/route.ts
@@ -1,7 +1,9 @@
 import { NextRequest } from 'next/server';
 
-const handler = (req: NextRequest) => {
-  console.log(req);
+const handler = async (req: NextRequest) => {
+  console.log(await req.json());
+  console.log(req.headers['X-Pusher-Key']);
+  console.log(req.headers['X-Pusher-Signature']);
 
   return Response.json({ a: 1 });
 };
