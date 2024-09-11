06c534209d823687d9311d0167e5ce878ef19979
peng-xiao-shuai <1612565136@qq.com>
2024-09-11 22:31:24 +0800

    fix: The channel is empty and cannot be deleted

--- src/app/api/pusher-hook/route.ts
+++ src/app/api/pusher-hook/route.ts
@@ -9,85 +9,88 @@ const handler = async (req: NextRequest) => {
     events: { name: string; channel: string }[];
   };
 
-  console.log('body', body);
-  console.log('user-agent', req.headers.get('user-agent'));
-  console.log('x-pusher-signature', req.headers.get('x-pusher-signature'));
+  if (req.headers.get('user-agent') === 'pusher-webhooks') {
+    const receivedSignature = req.headers.get('x-pusher-signature');
+    // const receivedKey = req.headers.get('x-pusher-key');
 
-  // if (req.headers.get('user-agent') === 'pusher-webhooks') {
-  //   const receivedSignature = req.headers.get('x-pusher-signature');
-  //   // const receivedKey = req.headers.get('x-pusher-key');
+    const payload = JSON.stringify(body);
 
-  //   const payload = JSON.stringify(body);
+    const expectedSignature = createHmac(
+      'sha256',
+      process.env.PUSHER_APP_SECRET!
+    )
+      .update(payload)
+      .digest('hex');
 
-  //   const expectedSignature = createHmac(
-  //     'sha256',
-  //     process.env.PUSHER_APP_SECRET!
-  //   )
-  //     .update(payload)
-  //     .digest('hex');
+    if (receivedSignature === expectedSignature) {
+      const payloadcms = await payloadPromise;
+      const remove = async (event: (typeof body)['events'][number]) => {
+        if (
+          event.channel.includes('presence-') &&
+          event.name === 'channel_vacated'
+        ) {
+          try {
+            const { docs: room } = await payloadcms.delete({
+              collection: 'rooms',
+              where: {
+                channel: { equals: event.channel },
+              },
+            });
 
-  //   console.log('expectedSignature', expectedSignature);
+            if (room[0].id) {
+              await payloadcms.delete({
+                collection: 'invite-link',
+                where: {
+                  roomId: { equals: room[0].id },
+                },
+              });
+            }
 
-  //   console.log(process.env.PAYLOAD_ADMIN_EMAIL, process.env.PAYLOAD_ADMIN_PWD);
+            console.log('删除：' + event.channel, room);
+          } catch (err) {
+            console.log(err, '删除报错');
+            throw err;
+          }
+        }
+      };
+      console.log('body.events', body.events);
 
-  //   if (receivedSignature === expectedSignature) {
-  const payloadcms = await payloadPromise;
-  const remove = async (event: (typeof body)['events'][number]) => {
-    if (
-      event.channel.includes('presence-') &&
-      event.name === 'channel_vacated'
-    ) {
       try {
-        console.log('开始登录', process.env.DATABASE_URL);
-        // const result = await payloadcms.login({
-        //   collection: 'users',
-        //   data: {
-        //     email: process.env.PAYLOAD_ADMIN_EMAIL!,
-        //     password: process.env.PAYLOAD_ADMIN_PWD!,
-        //   },
-        // });
-        // console.log('登录 result', result);
-
-        const { docs: room, errors } = await payloadcms.delete({
-          collection: 'rooms',
-          where: {
-            channel: { equals: event.channel },
-          },
+        body.events.forEach(async (event) => {
+          await remove(event);
         });
 
-        console.log('errors', errors);
-        console.log('删除：' + event.channel, room);
+        // 处理有效的 webhook 逻辑
+        return new Response(
+          JSON.stringify({
+            data: 'Webhook received',
+          }),
+          {
+            status: 200,
+          }
+        );
       } catch (err) {
-        console.log(err, '删除报错');
+        return new Response(
+          JSON.stringify({
+            data: 'Invalid webhook signature',
+          }),
+          {
+            status: 500,
+          }
+        );
       }
+    } else {
+      console.log('Invalid webhook');
+      return new Response(
+        JSON.stringify({
+          data: 'Invalid webhook signature',
+        }),
+        {
+          status: 401,
+        }
+      );
     }
-  };
-  console.log('body.events', body.events);
-
-  body.events.forEach((event) => {
-    remove(event);
-  });
-  // 处理有效的 webhook 逻辑
-  return new Response(
-    JSON.stringify({
-      data: 'Webhook received',
-    }),
-    {
-      status: 200,
-    }
-  );
-  // } else {
-  //   console.log('Invalid webhook');
-  //   return new Response(
-  //     JSON.stringify({
-  //       data: 'Invalid webhook signature',
-  //     }),
-  //     {
-  //       status: 401,
-  //     }
-  //   );
-  // }
-  // }
+  }
 };
 
 export { handler as POST };
